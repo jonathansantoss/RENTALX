0a5a6b9eff600ae659d8a2d9c311985eee617c14
Jonathan Gomes <jonathan.gomess1@gmail.com>
2022-09-10 13:50:38 -0300

    create users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,11 @@
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+
+import { Router } from "express";
+
+const usersRouters = Router();
+
+const createUserController = new CreateUserController();
+
+usersRouters.post("/", createUserController.handle);
+
+export { usersRouters };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,16 +1,23 @@
-import { container } from 'tsyringe';
+import { container } from "tsyringe";
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRespository } from '../../modules/cars/repositories/implementations/CategoriesRespository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRespository } from "../../modules/cars/repositories/implementations/CategoriesRespository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
-  'CategoriesRespository',
-  CategoriesRespository,
+    "CategoriesRespository",
+    CategoriesRespository
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-  'SpecificationsRepository',
-  SpecificationsRepository,
+    "SpecificationsRepository",
+    SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );
